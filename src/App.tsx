a62d44c0606b5043ad21dccd2bf43250d3d9289e
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-01-24 14:12:15 -0900

    Navegue entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import EstiloGlobal, { Container } from './styles'
 import store from './store/'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 const rotas = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ const rotas = createBrowserRouter([
   },
   {
     path: '/novo',
-    element: <h1>Nova Tarefa</h1>
+    element: <Cadastro />
   }
 ])
 function App() {
